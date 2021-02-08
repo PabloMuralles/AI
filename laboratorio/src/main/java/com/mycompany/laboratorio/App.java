070a63862ea413a4984ed54ed453a9ff1ddd052c
pablo <pablomuralles28@gmail.com>
2021-02-08 00:42:25 -0600

    revisar tiene erorres

--- laboratorio/src/main/java/com/mycompany/laboratorio/App.java
+++ laboratorio/src/main/java/com/mycompany/laboratorio/App.java
@@ -17,19 +17,32 @@ public class App
         var zona2 = new Node<Integer>(2);
         var zona5 = new Node<Integer>(5);
         var zona4 = new Node<Integer>(4);
+        var zona9 = new Node<Integer>(9);
         var zona10 = new Node<Integer>(10);
+        var zona12 = new Node<Integer>(12);
+        var zona13 = new Node<Integer>(13);
+        var zona14 = new Node<Integer>(14);
         var zona15 = new Node<Integer>(15);
         var zona16 = new Node<Integer>(16);
+        var zona21 = new Node<Integer>(21);
         
-        zona1.connect(zona2);
-        zona1.connect(zona5);
-        zona1.connect(zona4);
-        zona5.connect(zona4);
-        zona4.connect(zona10);
+        zona21.connect(zona12);
+        zona12.connect(zona13);
+        zona13.connect(zona9);
+        zona13.connect(zona14);
+        zona14.connect(zona10);
+        zona10.connect(zona9);
         zona10.connect(zona15);
-        zona15.connect(zona16);
+        zona10.connect(zona16);
+        zona9.connect(zona4);
+        zona9.connect(zona1);
+        zona4.connect(zona1);
+        zona4.connect(zona5);
+        zona1.connect(zona5);
+        zona1.connect(zona2);
+      
         
-        BFS.search(16,zona2);
+        BFS.search(16,zona21);
         
     }
 }
