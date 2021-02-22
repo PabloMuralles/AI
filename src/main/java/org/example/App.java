65f149779535d5c5e968a8865905d795412c872a
PabloMuralles <pablomuralles28@gmail.com>
2021-02-22 15:01:32 -0600

    lab terminado

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,32 +12,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        //CSP
-        //Variables
-        List<String> variables = List.of("Western Australia", "Nothern Territory",
-                "Queensland","South Australia", "New South Wales","Victoria","Tasmania");
-        //Dominios
-        Map<String, List<String>> domains = new HashMap<>();
-        for (var variable:variables ){
-            domains.put(variable, List.of("rojo","verde","azul"));
-        }
-        //Restricciones
-        CSP<String,String> problema = new CSP<>(variables,domains);
-        problema.addConstraint(new AustralianColoringConstraint("Western Australia","Nothern Territory"));
-        problema.addConstraint(new AustralianColoringConstraint("Western Australia","South Australia"));
-        problema.addConstraint(new AustralianColoringConstraint("Nothern Territory","South Australia"));
-        problema.addConstraint(new AustralianColoringConstraint("Nothern Territory","Queensland"));
-        problema.addConstraint(new AustralianColoringConstraint("South Australia","Queensland"));
-        problema.addConstraint(new AustralianColoringConstraint("New South Wales","Queensland"));
-        problema.addConstraint(new AustralianColoringConstraint("New South Wales","South Australia"));
-        problema.addConstraint(new AustralianColoringConstraint("Victoria","South Australia"));
-        problema.addConstraint(new AustralianColoringConstraint("New South Wales","Victoria"));
-        problema.addConstraint(new AustralianColoringConstraint("Tasmania","Victoria"));
-
-        var solution = problema.backtrack();
-
-
-        //System.out.println(solution);
+//        //CSP
+//        //Variables
+//        List<String> variables = List.of("Western Australia", "Nothern Territory",
+//                "Queensland","South Australia", "New South Wales","Victoria","Tasmania");
+//        //Dominios
+//        Map<String, List<String>> domains = new HashMap<>();
+//        for (var variable:variables ){
+//            domains.put(variable, List.of("rojo","verde","azul"));
+//        }
+//        //Restricciones
+//        CSP<String,String> problema = new CSP<>(variables,domains);
+//        problema.addConstraint(new AustralianColoringConstraint("Western Australia","Nothern Territory"));
+//        problema.addConstraint(new AustralianColoringConstraint("Western Australia","South Australia"));
+//        problema.addConstraint(new AustralianColoringConstraint("Nothern Territory","South Australia"));
+//        problema.addConstraint(new AustralianColoringConstraint("Nothern Territory","Queensland"));
+//        problema.addConstraint(new AustralianColoringConstraint("South Australia","Queensland"));
+//        problema.addConstraint(new AustralianColoringConstraint("New South Wales","Queensland"));
+//        problema.addConstraint(new AustralianColoringConstraint("New South Wales","South Australia"));
+//        problema.addConstraint(new AustralianColoringConstraint("Victoria","South Australia"));
+//        problema.addConstraint(new AustralianColoringConstraint("New South Wales","Victoria"));
+//        problema.addConstraint(new AustralianColoringConstraint("Tasmania","Victoria"));
+//
+//        var solution = problema.backtrack();
+//
+//
+//        System.out.println(solution);
 
         var objComplex = new ComplexConstraint();
 
