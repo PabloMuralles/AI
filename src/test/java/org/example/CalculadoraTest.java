2d447a867b4553820cddc2994587f0db377e4cd0
PabloMuralles <pablomuralles28@gmail.com>
2021-03-07 11:46:00 -0600

    todos los tipos de test

--- src/test/java/org/example/CalculadoraTest.java
+++ src/test/java/org/example/CalculadoraTest.java
@@ -1,20 +1,48 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 public class CalculadoraTest {
 
+    Calculadora calculadora;
+
+    @BeforeAll
+    public static void startup(){
+        System.out.println("Inicializacion global ----------");
+    }
+
+    @BeforeEach
+    public void init(){
+        calculadora = new Calculadora();
+        System.out.println("Inicializando test ---------");
+    }
     @Test
     public void probarSuma(){
-        //Resultado Esperado
-        double esperado = 25;
+        assertEquals(25.0,calculadora.sumar(5,20));
+        System.out.println("Sumando . . . ");
+    }
+    
+    @Test
+    public void probarResta(){
+        assertEquals(30.0,calculadora.restar(50,20));
+        System.out.println("Restando . . . ");
+    }
 
-        // Ejecutar y obtener
-        var calculador = new Calculadora();
-        var obtenido = calculador.sumar(5,20);
+    @Test
+    public void probarMultiplicacion(){
+        assertEquals(25.0,calculadora.multiplicar(5,5));
+        System.out.println("Multiplicando . . . ");
+    }
 
-        //Comparar esperado vs obtenido
-        assertEquals(esperado,obtenido);
+    @AfterEach
+    public void shutdown(){
+        System.out.println("Finalizando test ----------");
+    }
+
+    @AfterAll
+    public static void destroy(){
+        System.out.println("Finalizacion global ----------");
     }
 }
